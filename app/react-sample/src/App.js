87a3fec03bc4382ad3fe5b7988aa5a172074223b
吳郁心 <r12725043@ntu.edu.tw>
2024-04-12 16:05:31 +0800

    Reset App.js

--- app/react-sample/src/App.js
+++ app/react-sample/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import HeaderBar from "./components/HeaderBar";
-import NewActivity from './page/NewActivity';
 
 function App() {
   return (
     <div>
       <HeaderBar />
       <main>
-        <NewActivity/>
       </main>
     </div>
   );
