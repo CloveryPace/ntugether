7c169febf72285fd57d299e40b994e6b42c8159c
Angela <107647692+yushin2001@users.noreply.github.com>
2024-04-19 15:36:41 +0800

    Add ReviewBox

---
+++ app/react-sample/src/components/ReviewBox.js
@@ -0,0 +1,35 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
+import TextField from "@mui/material/TextField";
+import { useRef } from "react";
+
+export default function ReviewBox({data}) {
+    const Answer = useRef();
+    const handleSubmit = e => {
+        console.log(Answer.current?.value);
+    };  
+
+    const style = { 
+      border: '1.5px solid rgba(0, 0, 0, 0.1)',
+      padding: '2rem'
+    };
+
+    return (
+        <Grid item xs={12} md={4}>
+        <div className="box" style={style}>
+          <Stack direction="column" spacing={2}>
+            <Typography variant="h5" gutterBottom>立即加入</Typography>
+              <TextField
+                variant="outlined"
+                name="reviewreply"
+                label="輸入回答"
+                inputRef={Answer}
+                />
+              <Button variant="contained" color="primary" onClick={handleSubmit}> 送出加入請求 </Button> 
+          </Stack>
+        </div>
+        </Grid>
+    );
+}
