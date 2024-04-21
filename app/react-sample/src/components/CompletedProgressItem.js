976d70d41655130bb913b0f30332495f47909cca
Angela <107647692+yushin2001@users.noreply.github.com>
2024-04-22 01:45:09 +0800

    Add MyProgress and ProgressComponent

---
+++ app/react-sample/src/components/CompletedProgressItem.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { ListItem, ListItemText, Paper } from '@mui/material';
+import {useTheme} from '@mui/material';
+import Button from '@mui/material/Button';
+
+export default function CompletedProgressItem({ item, index }) {
+  const theme = useTheme();
+
+    return (
+      <Paper elevation={3} style={{ marginBottom: '10px', padding: '10px', backgroundColor: theme.palette.primary.light}}>
+        <ListItem>
+          
+          <ListItemText 
+            primary={
+                <>  
+                    {"進度名稱：" + item.name} 
+                    <br/>
+                    {"完成時間：" + item.time}
+                    <br/>
+                    {"進度細節：" + item.detail}
+                </>
+              } />
+        </ListItem>
+        <div style={{ padding: 5 }}>
+          <Button variant="contained" color="primary">編輯</Button>
+        </div>
+      </Paper>
+    );
+}
