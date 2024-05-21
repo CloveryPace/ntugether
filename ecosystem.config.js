b4472cd46d5ad2abf2ba9051f7e19525789143c7
CloveryPace <128057141+CloveryPace@users.noreply.github.com>
2024-05-21 14:48:01 +0000

    Prefix log with time

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
         name: "sdm-backend",
         script: "./index.js",
         watch: true,
+        time: true,
+        log_date_format: "YYYY-MM-DD HH:mm Z",
         ignore_watch: ["node_modules"]
       }
   ]
