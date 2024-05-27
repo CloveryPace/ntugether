10f35ba4e80cc5a0cab1b9d249d54a940483d692
Angela <107647692+yushin2001@users.noreply.github.com>
2024-05-27 23:15:07 +0800

    Recent activity on homepage and show more button function completed

--- app/react-sample/src/components/RecentActivityList.js
+++ app/react-sample/src/components/RecentActivityList.js
@@ -2,27 +2,39 @@ import React, { useState } from 'react';
 import { List, Typography, Button } from '@mui/material';
 import RecentActivityItem from './RecentActivityItem';
 import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
+import { getAuthToken } from '../utils';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { API_CREATE_ACTIVITY } from '../global/constants';
 
 export default function RecentActivityList() {
   const { t, i18n } = useTranslation();
   const initialDisplayCount = 2;
   const incrementCount = 3;
+  const [recentActivity, setRecentActivity] = useState([]);
 
-  const recentActivity = [
-    { date: '4/5', time: '11:00-12:00', name: '慢跑' , location: '大安森林公園'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'},
-    { date: '4/11', time: '11:00-13:00', name: '籃球', location: '台大新體'}
-  ];
+  useEffect(() => {
+    const token = getAuthToken();
+    const config = {
+        headers: { 
+          authorization: `Bearer ${token}`,
+        },
+        params: { 
+          start_date: dayjs().toJSON()
+        } 
+    };
+    //取得最近活動
+    axios.get(API_CREATE_ACTIVITY, config)
+      .then(function (res) {
+        console.log(res.data);
+        setRecentActivity(res.data);
+      })
+      .catch(function (err) {
+        console.log(err);
+        alert("error");
+      });
+    }, []);
 
   const [displayCount, setDisplayCount] = useState(initialDisplayCount);
 
